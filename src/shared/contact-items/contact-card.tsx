9a38ea5a03e66ea9ce4a4b03a85b425b09116908
sys333@india <jeswinshijo@sodisys.io>
2025-07-21 22:22:46 +0530

    changes in cntact card

---
+++ src/shared/contact-items/contact-card.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Stylesheet from "./contact-card.module.scss";
+
+const ContactCard = () => {
+  return (
+    <div className={Stylesheet.contactCard}>
+      <h2 className={Stylesheet.title}>You Have Any Questions?</h2>
+      <p className={Stylesheet.text}>
+        our team will answer all your questions. <br />
+        we ensure a quick response
+      </p>
+      <button className={Stylesheet.button}>
+        {/* Contact Us <ArrowRight size={16} /> */}
+      </button>
+    </div>
+  );
+};
+
+export default ContactCard;

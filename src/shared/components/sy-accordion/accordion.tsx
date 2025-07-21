8052351d27ebd8b88ddbad2562e6dcb7a5f9aa0b
sys333@india <jeswinshijo@sodisys.io>
2025-07-21 22:09:24 +0530

    changes in font style and accordion

---
+++ src/shared/components/sy-accordion/accordion.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useRef, useEffect } from "react";
+import Stylesheet from "./style.module.scss";
+
+type Props = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const Accordion = ({ title, children }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState("0px");
+
+  const toggleAccordion = () => setIsOpen(!isOpen);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      setHeight(isOpen ? `${contentRef.current.scrollHeight}px` : "0px");
+    }
+  }, [isOpen]);
+
+  return (
+    <div className={`${Stylesheet.accordionItem} ${isOpen ? Stylesheet.open : ""}`}>
+      <button className={Stylesheet.accordionHeader} onClick={toggleAccordion}>
+        <span className={Stylesheet.title}>{title}</span>
+        <span className={Stylesheet.icon}>{isOpen ? "–" : "+"}</span>
+      </button>
+      <div
+        ref={contentRef}
+        className={Stylesheet.accordionContent}
+        style={{ maxHeight: height }}
+      >
+        <div className={Stylesheet.contentInner}><span className={Stylesheet.children}>{children}</span></div>
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
